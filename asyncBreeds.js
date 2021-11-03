e95c3dfc77009c1e2cbc53b9c58e0490e3a707a3
Kevin Mason <krmason2020@gmail.com>
2021-11-03 00:40:50 +0000

    created test files for async testing

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,11 +1,12 @@
 // asyncBreeds.js
 const fs = require("fs");
 
-const breedDetailsFromFile = function(breed, callback) {
+const breedDetailsFromFile = function (breed, callback) {
   console.log("breedDetailsFromFile: Calling readFile...");
   fs.readFile(`./data/${breed}.txt`, "utf8", (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     callback(data);
+    if (!error) return data;
   });
 };
 
